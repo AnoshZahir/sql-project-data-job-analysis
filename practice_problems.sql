de046b11f69d371bcd2bdea3ed79a4e3629b2554
azazgit <anoshzahir@hotmail.com>
2024-09-10 13:09:21 +0100

    prob2: adjusted query to account for timezone change

--- practice_problems.sql
+++ practice_problems.sql
@@ -17,12 +17,12 @@ GROUP BY
 Write a query to count the number of job postings for each month in 2023, adjusting the job_posted_date to be in "America/New_York" time zone before extracting (hint) the month.Assume the job_posted_date is stored in UTC. Group by order by the month.
 '''
 SELECT 
-   EXTRACT(MONTH FROM job_posted_date) AS "month",
+   EXTRACT(MONTH FROM job_posted_date AT TIME ZONE 'UTC' AT TIME ZONE 'America/New_York') AS "month",
    count(job_id) AS "Number of jobs"
 FROM
    job_postings_fact
 WHERE
-   EXTRACT(YEAR FROM job_posted_date) = 2023
+   EXTRACT(YEAR FROM job_posted_date AT TIME ZONE 'UTC' AT TIME ZONE 'America/New_York') = 2023
 GROUP BY
    "month"
 ORDER BY
