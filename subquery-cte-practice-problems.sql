4ba06543523624149e2f65f4eac1c3380e3e1419
azazgit <anoshzahir@hotmail.com>
2024-09-12 13:42:48 +0100

    practice prob1 initiated.

---
+++ subquery-cte-practice-problems.sql
@@ -0,0 +1,3 @@
+'''Practice problem 1
+Identify the top 5 skills that are most frequently mentioned in job postings. Use a subquery to find the skill IDs with the highest counts in the skills_job_dim table and then join this result with the skills_dim table to get the skill names.
+'''
