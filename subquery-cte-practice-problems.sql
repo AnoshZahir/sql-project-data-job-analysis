5a4ef24a951da8bebd17ec4fe17cadda6077149a
azazgit <anoshzahir@hotmail.com>
2024-09-12 13:55:12 +0100

    practice prob1: count of of jobs for each skill added.

--- subquery-cte-practice-problems.sql
+++ subquery-cte-practice-problems.sql
@@ -1,3 +1,14 @@
 '''Practice problem 1
 Identify the top 5 skills that are most frequently mentioned in job postings. Use a subquery to find the skill IDs with the highest counts in the skills_job_dim table and then join this result with the skills_dim table to get the skill names.
 '''
+
+SELECT * FROM skills_job_dim LIMIT 5;
+SELECT * FROM skills_dim LIMIT 5;
+
+SELECT 
+    skill_id,
+    count(job_id)
+FROM
+    skills_job_dim
+GROUP BY
+    skill_id
