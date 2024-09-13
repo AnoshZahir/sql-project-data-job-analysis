0df7f87994086da45cbd60eda7a75519193c7832
azazgit <anoshzahir@hotmail.com>
2024-09-13 15:38:58 +0100

    pp1: comments line breaks removed.

--- subquery-cte-practice-problems.sql
+++ subquery-cte-practice-problems.sql
@@ -1,7 +1,6 @@
 '''Practice problem 1
 Identify the top 5 skills that are most frequently mentioned in job postings. Use a subquery to find the skill IDs with the highest counts in the skills_job_dim table and then join this result with the skills_dim table to get the skill names.
 '''
-
 -- Step 1: View the data from the two tables to know what I am working with.
 -- SELECT * FROM skills_job_dim LIMIT 5;
 -- SELECT * FROM skills_dim LIMIT 5;
